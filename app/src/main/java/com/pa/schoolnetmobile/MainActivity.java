eb8c8144c0f7aeffa84de26e779d2bbd4cb4b2cd
Felipe <felipe.lavandeira@gmail.com>
2019-04-21 18:57:16 -0300

    Criando a área de login

--- app/src/main/java/com/pa/schoolnetmobile/MainActivity.java
+++ app/src/main/java/com/pa/schoolnetmobile/MainActivity.java
@@ -2,6 +2,8 @@ package com.pa.schoolnetmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        Toast.makeText(this, "Botão de login Pressionado", Toast.LENGTH_SHORT).show();
     }
 }
