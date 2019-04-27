c5fd6e6c2c88089386fd1ca73d71c128b380469c
Felipe <felipe.lavandeira@gmail.com>
2019-04-27 16:47:14 -0300

    Desenvolvendo o frontend da Actiity de Atividades Diárias

--- app/src/main/java/com/pa/schoolnetmobile/InitialActivity.java
+++ app/src/main/java/com/pa/schoolnetmobile/InitialActivity.java
@@ -2,6 +2,13 @@ package com.pa.schoolnetmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class InitialActivity extends AppCompatActivity {
 
@@ -9,5 +16,17 @@ public class InitialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
+        TextView txtView = findViewById(R.id.txt_day);
+        txtView.setText(String.format("Atividades do dia %s", formatDate()));
+    }
+
+    private String formatDate(){
+        Date today = new Date();
+        today.getTime();
+        DateFormat dayFormat = new SimpleDateFormat("dd");
+        DateFormat monthFormat = new SimpleDateFormat("MMMM");
+        String dayText = dayFormat.format(today);
+        String monthText = monthFormat.format(today).substring(0,1).toUpperCase() + monthFormat.format(today).substring(1);
+        return dayText + " de " + monthText;
     }
 }
--- app/src/main/java/com/pa/schoolnetmobile/MainActivity.java
+++ app/src/main/java/com/pa/schoolnetmobile/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pa.schoolnetmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
-        Toast.makeText(this, "Botão de login Pressionado", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, InitialActivity.class);
+        startActivity(intent);
     }
 }
